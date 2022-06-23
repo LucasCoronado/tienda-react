38eb1b8803ec73d840b5a675f8cafc136b866226
Lucas Coronado <coronadolucasm@gmail.com>
2022-06-23 20:42:26 -0300

    Desafio Landing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import NavBar from "./Components/NavBar/NavBar"
+import ItemListContainer from "./Components/ItemListContainer/ItemListContainer"
 
 const App = () =>{
   return (
+    <>
     <NavBar/>
+    <ItemListContainer greeting="Catalogo no disponible"/>
+    </>
+
   )
 }
 
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,19 +1,19 @@
 import React from "react";
 import logo from "../../assets/logo.png"
 import "./NavBar.css"
-
+import CartWidget from "../CartWidget/CartWidget";
 
 const NavBar = () => {
     return (
         <nav>
             <img src={logo} alt="logo" />
-            <h1 class="navTitle">Tienda Online</h1>
-            <div class="navItems">
+            <h1 className="navTitle">Tienda Online</h1>
+            <div className="navItems">
                 <a href=".">Inicio</a>
                 <a href=".">Productos</a>
                 <a href=".">Contacto</a>
             </div>
-            
+            <CartWidget/>
         </nav >
     )
 }
